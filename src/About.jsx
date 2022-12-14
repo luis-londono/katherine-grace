59facf50d39a1db127dd10f4dec5bbd243e3a788
Luis Londono <lulondono08@gmail.com>
2022-12-13 20:56:40 -0600

    updated about page

--- src/About.jsx
+++ src/About.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
-import Carolina from './images/carolina.jpg';
+import React from "react";
+// import Carolina from "./images/carolina.jpg";
+import Engagement from "./images/katherine-engagement-small.jpg";
 
 const About = () => {
-    return (
-        <>
-            <p>about kg0d</p>
-            <img id="carolina" src={Carolina} alt="carolina"></img>
-        </>
-    );
+  return (
+    <>
+      <h2>About</h2>
+        
+        <div className="about-wrapper">
+            <div className="row">
+                <div className="col-md-2" />
+                <div className="col-md-4">
+                <img
+                    id="engagement"
+                    src={Engagement}
+                    alt="engagement"
+                ></img>
+                </div>
+                <div className="col-md-4 about-description">
+                    <span className="emoji">👋</span> All of the text goes here. Testing the tell all stuff. All of the text goes here. Testing the tell all stuff. All of the text goes here. Testing the tell all stuff. 
+                    All of the text goes here. Testing the tell all stuff. All of the text goes here. Testing the tell all stuff. All of the text goes here. Testing the tell all stuff. 
+                    All of the text goes here. Testing the tell all stuff. All of the text goes here. Testing the tell all stuff. All of the text goes here. Testing the tell all stuff.
+                    All of the text goes here. Testing the tell all stuff. All of the text goes here. Testing the tell all stuff. All of the text goes here. Testing the tell all stuff.
+                    All of the text goes here. Testing the tell all stuff. All of the text goes here. Testing the tell all stuff. All of the text goes here. Testing the tell all stuff. 💇‍♀️
+
+                </div>
+            </div>
+        </div>
+
+      
+    </>
+  );
 };
 
 export default About;
