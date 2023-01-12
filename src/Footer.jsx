01c9d765d357168cb3afebc0a35bb2ac2b0d19ea
Luis Londono <lulondono08@gmail.com>
2023-01-11 20:36:15 -0600

    refactor css

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -9,6 +9,35 @@ const Footer = () => {
           Katherine Grace Hair <span id="period">•</span> Est 2015
         </h2>
         <p>&copy; {year} All Rights Reserved</p>
+        <a
+          className="social"
+          href="mailto:kglondono@gmail.com"
+          target="_blank"
+          without="true"
+          rel="noreferrer"
+        >
+          <i className="fa fa-envelope" aria-hidden="true"></i>
+        </a>
+        &nbsp;
+        <a
+          className="social"
+          href="https://www.instagram.com/ktgodberry/"
+          target="_blank"
+          without="true"
+          rel="noreferrer"
+        >
+          <i className="fa fa-instagram" aria-hidden="true"></i>
+        </a>
+        &nbsp;
+        <a
+          className="social"
+          href="https://www.facebook.com/katie.godbey.1"
+          target="_blank"
+          without="true"
+          rel="noreferrer"
+        >
+          <i className="fa fa-facebook" aria-hidden="true"></i>
+        </a>
       </div>
     </>
   );
--- src/Navigation_.jsx
+++ src/Navigation_.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
-import "./Nav.scss";
+// import "./Nav.scss";
 
 const Navigation_ = () => {
   const [isMobile, setIsMobile] = useState(false);
@@ -77,76 +77,6 @@ const Navigation_ = () => {
           </div>
         </nav>
       </div>
-
-      {/* 
-      <div className="row header">
-        <div className="col-md-2 col-sm-2 log">
-          <img id="logo" src={logo} alt="logo"></img>
-        </div>
-
-        <div className="col-md-8 col-sm-2 navigation">
-          <nav className="nav-bar">
-            <ul className={navOpen ? "nav-list-mobile" : "nav-list"}>
-              <NavLink className="nav-link" to={"/"}>
-                Home
-              </NavLink>
-            </ul>
-            <ul className={navOpen ? "nav-list-mobile" : "nav-list"}>
-              <NavLink className="nav-link" to={"/services"}>
-                Services
-              </NavLink>
-            </ul>
-            <ul className={navOpen ? "nav-list-mobile" : "nav-list"}>
-              <NavLink className="nav-link" to={"/gallery"}>
-                Gallery
-              </NavLink>
-            </ul>
-            <ul className={navOpen ? "nav-list-mobile" : "nav-list"}>
-              <NavLink className="nav-link" to={"/about"}>
-                About
-              </NavLink>
-            </ul>
-          </nav>
-          <button className="col-sm-2 nav-mobile" onClick={navOnClick}>
-            {navOpen ? (
-              <i className="fa fa-times" />
-            ) : (
-              <i className="mkdf-icon-font-awesome fa fa-bars" />
-            )}
-          </button>
-        </div>
-        <div className="col-md-2 navigation">
-          <a
-            className="social"
-            href="mailto:kglondono@gmail.com"
-            target="_blank"
-            without="true"
-            rel="noreferrer"
-          >
-            <i className="fa fa-envelope" aria-hidden="true"></i>
-          </a>
-          &nbsp;
-          <a
-            className="social"
-            href="https://www.instagram.com/ktgodberry/"
-            target="_blank"
-            without="true"
-            rel="noreferrer"
-          >
-            <i className="fa fa-instagram" aria-hidden="true"></i>
-          </a>
-          &nbsp;
-          <a
-            className="social"
-            href="https://www.facebook.com/katie.godbey.1"
-            target="_blank"
-            without="true"
-            rel="noreferrer"
-          >
-            <i className="fa fa-facebook" aria-hidden="true"></i>
-          </a>
-        </div>
-      </div> */}
     </>
   );
 };
