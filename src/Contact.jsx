62388ec2ebad2454c8d2c9baec4164725824f1ca
Luis Londono <lulondono08@gmail.com>
2023-01-14 20:12:03 -0600

    contact pt2

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,23 +1,39 @@
-import React from 'react';
+import React from "react";
 
 const Contact = () => {
   return (
     <>
       <table style={{ display: "inline-table" }}>
         <tbody>
-          <tr >
-            <td><strong>Phone:</strong></td>
+          <tr>
+            <td>
+              <strong>Phone:</strong>
+            </td>
             <td>&nbsp;&nbsp;</td>
             <td className="float-start">(816) 217-8928</td>
           </tr>
           <tr>
-            <td><strong>Email:</strong></td>
+            <td>
+              <strong>Email:</strong>
+            </td>
             <td>&nbsp;&nbsp;</td>
-            <td className="float-start">kglondono@gmail.com</td>
+            <td className="float-start">
+              {" "}
+              <a
+                // className={className}
+                style={{ color: "white", textDecoration: "none" }}
+                href="mailto:kglondono@gmail.com"
+                target="_blank"
+                without="true"
+                rel="noreferrer"
+              >
+                kglondono@gmail.com
+              </a>
+            </td>
           </tr>
         </tbody>
       </table>
-{/* 
+      {/* 
       <p><strong>Phone:</strong> </p>
       <p><strong>Email:</strong> kglondono@gmail.com</p> */}
     </>
