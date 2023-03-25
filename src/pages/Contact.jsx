d735d6a861420fa9aaa859df3022561a45eb8f4a
Luis Londono <lulondono08@gmail.com>
2023-03-25 10:51:10 -0500

    update to not found page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,8 @@
-import React, { useRef } from "react";
+import React from "react";
 import Frame from '../components/Frame'
 
 const Contact = () => {
-  const iframeRef = useRef(null);
+  // const iframeRef = useRef(null);
 
   // useEffect(() => {
 
@@ -49,7 +49,7 @@ const Contact = () => {
         <div className="col-lg-6">
           {/* {iframeRef ? <Spinner /> : null} */}
           <Frame 
-              ref={iframeRef}
+              // ref={iframeRef}
               title="bijin"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3100.5964583478717!2d-94.63379458473642!3d39.001705379554735!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87c0ee96764f3e63%3A0xa462b45bee717954!2sbijin%20salon%20%26%20spa!5e0!3m2!1sen!2sus!4v1673753317651!5m2!1sen!2sus"
               width="325"
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const NotFound = () => {
-    return (
-        <div>
-            <p>Whoopsies, this page does not exists!</p>
-        </div>
-    );
+  return (
+    <div>
+      <i
+        className="fa fa-exclamation-triangle"
+        style={{ fontSize: "80px" }}
+        aria-hidden="true"
+      ></i>
+      <p>Whoopsies, this page does not exists!</p>
+    </div>
+  );
 };
 
 export default NotFound;
